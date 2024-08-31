0d2bc8312891d250e9b28b70e12a78a4914f1dac
Ray <rey@gmail.com>
2024-08-31 22:27:15 +0330

    change average

--- average.py
+++ average.py
@@ -1,8 +1,8 @@
-n=int(input("n="))
-s=0.0
-for i in range(1,n+1):
-    x=float(input("number"+str(i)+"="))
-    s+=x
-m=s/n
-print("mean""=",m)
-
+x=int (input("how many numbers:"))
+sum=0
+for i in range(0,x):
+    y=float(input("enter number:"))
+    sum=sum+y
+    z=sum/x
+print(z)         
+    
